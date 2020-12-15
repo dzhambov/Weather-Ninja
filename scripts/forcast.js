08c6f691ec8c46ff33ff0f401e3d694a5f9609a7
Hristo Dzhambov <dzhambov@hotmail.com>
2020-12-15 14:20:52 -0500

    add get city api call.

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,2 +1,16 @@
-const key = '8wRWxmt2VioJdeImSqEGQ9mALKMm8aGu
-';
+const key = '8wRWxmt2VioJdeImSqEGQ9mALKMm8aGu';
+
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+}
+
+getCity('beverly')
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
