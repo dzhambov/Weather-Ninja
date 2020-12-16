fd22d0250e6e76370d2d91290549703e14e6d8cf
Hristo Dzhambov <dzhambov@hotmail.com>
2020-12-16 15:55:47 -0500

    add updating to the UI.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,33 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  // Update details template
+  details.innerHTML = ` 
+  <h5 class="my-3">${cityDets.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${weather.Temperature.Metric.Value}</span>
+    <span>&deg;c</span>
+  </div>
+  `;
+  if(card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
+
 
 const updateCity = async (city) => {
 
 const cityDets = await getCity(city);
 const weather = await getWeather(cityDets.Key);
 
-  return {
-    cityDets: cityDets,
-    weather: weather
-  };
+  return { cityDets, weather };
 };
 
 cityForm.addEventListener('submit', e => {
@@ -20,6 +39,6 @@ cityForm.addEventListener('submit', e => {
 
   // Update that UI with new City
   updateCity(city)
-  .then(data => console.log(data))
+  .then(data => updateUI(data))
   .catch(err => console.log(err))
 });
